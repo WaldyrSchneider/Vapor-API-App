13bb06b26818e827c6fcb3b58100268c890b13f5
Waldyr Schneider <Waldyr.Schneider@edu.pucrs.br>
2023-08-29 16:37:15 -0300

    Initial Commit

---
+++ Vapor-API-App/Vapor_API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_API_AppApp.swift
+//  Vapor-API-App
+//
+//  Created by Waldyr Schneider on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
